afb3bbf3c2b59d341bf85b98c7a762147800533c
xueyong <1085523029@qq.com>
2022-02-06 00:26:58 +0800

    feat: upload builds

---
+++ client/build/precache-manifest.22c16c9d6b2630b8ec456616f40b6805.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70158fe7e2c74eb48f4dac9479b3f02e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7246cf8976225b2379d",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1a61469ebbbaf96220d3",
+    "url": "/static/js/2.4d32b0e5.chunk.js"
+  },
+  {
+    "revision": "e7246cf8976225b2379d",
+    "url": "/static/js/main.3efc781a.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
