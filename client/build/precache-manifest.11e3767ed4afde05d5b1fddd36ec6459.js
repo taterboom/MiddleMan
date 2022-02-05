2895fba28ac47a318005748bd3c10b22d6e817f7
xueyong <1085523029@qq.com>
2022-02-06 00:37:29 +0800

    feat: update builds path

---
+++ client/build/precache-manifest.11e3767ed4afde05d5b1fddd36ec6459.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bebfa747d386bbb9caf088e8d558cf92",
+    "url": "/MiddleMan/client/index.html"
+  },
+  {
+    "revision": "937899e756b7d0ba02ff",
+    "url": "/MiddleMan/client/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5ff7c788d6f5766976ed",
+    "url": "/MiddleMan/client/static/js/2.cac448f0.chunk.js"
+  },
+  {
+    "revision": "937899e756b7d0ba02ff",
+    "url": "/MiddleMan/client/static/js/main.835f9d23.chunk.js"
+  },
+  {
+    "revision": "0dba47439cf5ee17f00d",
+    "url": "/MiddleMan/client/static/js/runtime-main.4480226b.js"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.22c16c9d6b2630b8ec456616f40b6805.js"
+  "/MiddleMan/client/precache-manifest.11e3767ed4afde05d5b1fddd36ec6459.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MiddleMan/client/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
